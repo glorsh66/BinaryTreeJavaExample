7e4ed147501501cb7a0df9050b752f786cb3b76d
glorsh66 <30538582+glorsh66@users.noreply.github.com>
2018-09-07 15:04:35 +0300

    The very first commit

---
+++ BTree.java
@@ -0,0 +1,219 @@
+import jdk.nashorn.api.tree.Tree;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class BTree {
+    public Node root;
+    public int stepCounter =0;
+    public int nodeCount=0;
+    public ArrayList<Integer> results = new ArrayList<Integer>();
+
+    public BTree() {
+        this.root = null;
+    }
+
+    public void add(Node n)
+    {
+        if (this.root==null) {this.root=n; this.nodeCount++; return;}
+
+        boolean flag=true;
+        Node cur = this.root;
+        do {
+            //if there already exist this element
+            if (cur.i==n.i) {flag=false;break;}
+
+            //If n is less than root
+            if (n.i< cur.i) {
+                if (cur.left==null) {cur.left=n; this.nodeCount++; flag=false;break;}
+                else {cur=cur.left;flag=true; continue;}
+            }
+            //If n is less than root
+            if (n.i> cur.i) {
+                if (cur.right==null) {cur.right=n; this.nodeCount++; flag=false;break;}
+                else {cur=cur.right;flag=true; continue;}
+            }
+
+        }while (flag);
+    }
+
+
+
+
+    public boolean findForLoop(int i)
+    {
+        this.stepCounter =0;
+        Node cur = this.root;
+        boolean flag = true;
+
+        do {
+            this.stepCounter++;
+            if (cur==null) return false;
+            if (cur.i==i) return true;
+
+
+            if (i<cur.i) cur=cur.left;
+            else  cur=cur.right;
+
+        }while (flag);
+        return false;
+    }
+
+
+    public boolean findRecursive(Node cur, int i)
+    {
+        if (cur==null) return false;
+
+        if (cur.i==i) return true;
+
+        boolean b;
+        if (i<cur.i) {
+            b=findRecursive(cur.left,i);
+            return b;
+        }
+        else  {
+            b=findRecursive(cur.right,i);
+            return b;
+        }
+
+
+    }
+
+
+
+
+    public void addRecursiveWrapper(Node value)    {
+        this.root=addRecursive(this.root,value, this);
+    }
+
+
+
+    //Don't increment coutn of elements in the BTree
+    private Node addRecursive(Node cur, Node value, BTree t)
+    {
+        if (cur== null) {
+            this.nodeCount++;
+            return value;
+        }
+
+        if (value.i < cur.i) {
+            cur.left = addRecursive(cur.left,value,t);
+        } else if (value.i > cur.i)
+        {
+            cur.right= addRecursive(cur.right,value,t);
+        }
+        return cur;
+    }
+
+
+
+    public int maxDepthRecursion(Node n)
+    {
+        if (n==null) return 0;
+
+        int maxleft = maxDepthRecursion(n.left);
+        int maxright = maxDepthRecursion(n.right);
+
+        int biggest = Math.max(maxleft,maxright);
+        return biggest+1;
+    }
+
+
+    public ArrayList<Integer> traverseFromZeroToMaxForLoopInOrder()
+    {
+        this.stepCounter=0;
+        Node cur = this.root;
+        if (cur==null) return null;
+
+        ArrayList<Integer> ret = new ArrayList<Integer>();
+        Stack<Node> s = new Stack<Node>();
+        //s.push(cur);
+
+        do {
+
+
+         if (cur!=null)
+         {
+             s.push(cur);
+             cur=cur.left;
+         }else {
+           Node el = s.pop();
+           this.stepCounter++;
+           ret.add(el.i);
+           cur = el.right;
+         }
+        }while ((cur!=null) || (!(s.size()==0)));
+        return ret;
+
+    }
+
+    public void traverseFromZeroToMaxRecurrsionInOrder(Node n) {
+        if (n != null) {
+            traverseFromZeroToMaxRecurrsionInOrder(n.left);
+            this.results.add(n.i);
+            this.stepCounter++;
+            traverseFromZeroToMaxRecurrsionInOrder(n.right);
+        }
+    }
+
+
+    public void traverseFromMaxToZeroRecurrsionInOrder(Node n) {
+        if (n != null) {
+            traverseFromMaxToZeroRecurrsionInOrder(n.right);
+            this.results.add(n.i);
+            this.stepCounter++;
+            traverseFromMaxToZeroRecurrsionInOrder(n.left);
+
+
+        }
+    }
+
+
+    public ArrayList<Integer> traverseFromMaxToZeroForLoopInOrder()
+    {
+        this.stepCounter=0;
+        Node cur = this.root;
+        if (cur==null) return null;
+
+        ArrayList<Integer> ret = new ArrayList<Integer>();
+        Stack<Node> s = new Stack<Node>();
+        //s.push(cur);
+
+        do {
+            if (cur!=null)
+            {
+                s.push(cur);
+                cur=cur.right;
+            }else {
+                Node el = s.pop();
+                this.stepCounter++;
+                ret.add(el.i);
+                cur = el.left;
+            }
+        }while ((cur!=null) || (!(s.size()==0)));
+        return ret;
+
+    }
+
+//Pre-order traversal visits first the root node, then the left subtree, and finally the right subtree:
+    public void traversePreOrderRecursion(Node n) {
+        if (n != null) {
+            this.results.add(n.i);
+            this.stepCounter++;
+            traversePreOrderRecursion(n.left);
+            traversePreOrderRecursion(n.right);
+        }
+    }
+//Post-order traversal visits the left subtree, the right subtree, and the root node at the end:
+    public void traversePostOrder(Node n) {
+        if (n != null) {
+            traversePostOrder(n.left);
+            traversePostOrder(n.right);
+            this.results.add(n.i);
+            this.stepCounter++;
+        }
+    }
+
+
+
+}
---
+++ Node.java
@@ -0,0 +1,9 @@
+public class Node {
+    public int i;
+    public Node left;
+    public Node right;
+
+    public Node(int i) {
+        this.i = i;
+    }
+}
